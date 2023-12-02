64eaebb17662d2dba526ace1ae537a54a4b59f83
Edward Smale <essmale2005@gmail.com>
2023-12-02 14:27:13 +0000

    Add part 2 and vscode settings for Nim

---
+++ day2/part1.nim
@@ -0,0 +1,33 @@
+import std/strutils
+import std/sequtils
+import std/os
+
+type Round = ( int, int, int )
+
+proc parse_as_round(round: string, limits: Round): bool =
+    var hands = round.split ", "
+    for hand in hands:
+        var parts = hand.split ' '
+        if parts[1] == "red" and parseInt(parts[0]) > limits[0]: return false
+        if parts[1] == "green" and parseInt(parts[0]) > limits[1]: return false
+        if parts[1] == "blue" and parseInt(parts[0]) > limits[2]: return false
+    return true
+
+proc parse_as_game(game: string, limits: Round): int =
+    var parts = game.split ": " 
+    var rounds = parts[1].split "; "
+    return if rounds.all (proc(round: string): bool = round.parse_as_round limits):
+        parseInt (parts[0].split ' ')[1] else: 0
+
+var fileData = readFile paramStr 1
+var games = file_data.split '\n'
+var limits = ( red: 12, green: 13, blue: 14 )
+var gameIdTotal = 0
+for game in games:
+    gameIdTotal += game.parse_as_game limits
+
+echo gameIdTotal
+
+
+
+
---
+++ day2/part2.nim
@@ -0,0 +1,34 @@
+import std/strutils
+import std/os
+
+type Round = ( int, int, int )
+
+proc parse_as_round(round: string, limits: var Round): void =
+    var hands = round.split ", "
+    for hand in hands:
+        var parts = hand.split ' '
+        case parts[1][0]:
+            of 'r':
+                if parseInt(parts[0]) > limits[0]: limits[0] = parseInt(parts[0])
+            of 'g':
+                if parseInt(parts[0]) > limits[1]: limits[1] = parseInt(parts[0])
+            of 'b':
+                if parseInt(parts[0]) > limits[2]: limits[2] = parseInt(parts[0])
+            else: discard
+
+proc parse_as_game(game: string, limits: var Round): void =
+    var parts = game.split ": " 
+    var rounds = parts[1].split "; "
+    for round in rounds:
+        round.parse_as_round limits
+
+var fileData = readFile paramStr 1
+var games = file_data.split '\n'
+var totalPower = 0
+for game in games:
+    var limits = ( 0, 0, 0 )
+    game.parse_as_game limits
+    totalPower += limits[0] * limits[1] * limits[2]
+
+echo totalPower
+
